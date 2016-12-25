ccef10ff3ca473679f68610ca95bc709ebd6e989
Faik TURAN <faikturan@users.noreply.github.com>
2016-12-25 15:59:11 +0300

    Add files via upload

--- Login.java
+++ Login.java
@@ -1,8 +1,10 @@
 package com.faikturan;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -19,33 +21,89 @@ public class Login extends JFrame implements Database_Interface, ActionListener 
 	private JButton loginBtn;
 	public static String user = "";
 	private String password = "";
-	
+
 	public Login() {
 		super("Login Screen");
-		
-		//Initialize connection
+
+		// Initialize connection
 		ConnectionHelper.DBConnect();
-		
-		//construct compenets
+
+		// construct compenets
 		userNameLbl = new JLabel("Username: ");
 		passwordLbl = new JLabel("Password: ");
 		usernameTf = new JTextField(5);
 		passwordTf = new JPasswordField(5);
 		loginBtn = new JButton("Login");
+
+		// Add logo image to label
+		ImageIcon image = new ImageIcon("resources\\airline.jpg");
+		logoLbl = new JLabel("", image, JLabel.CENTER);
 		
-		//Add logo image to label
+		//adjust size and set layout
+		setSize(450, 250);
+		setLayout(null);
+		
+		//add compenents
+		add(userNameLbl);
+		add(passwordLbl);
+		add(usernameTf);
+		add(passwordTf);
+		add(loginBtn);
+		add(logoLbl);
+		
+		//set compenent bounds (only needed by Absolute Positioning)
+		userNameLbl.setBounds(100, 105, 75, 25);
+		passwordLbl.setBounds(100, 135, 75, 25);
+		usernameTf.setBounds(170, 105, 130, 25);
+		passwordTf.setBounds(170, 135, 130, 25);
+		loginBtn.setBounds(170, 170, 100, 25);
+		logoLbl.setBounds(60, 10, 300, 75);
+		
+		//add actionListener to Login Button
+		loginBtn.addActionListener(this);
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		getContentPane().setBackground(Color.WHITE);
+		setLocationRelativeTo(null);
+		setVisible(true);
 		
 	}
-	
+
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+	public void actionPerformed(ActionEvent event) {
+		Object target = event.getSource();
+		
+		if (target == loginBtn) {
+			initialise();
+		}
 
 	}
 
+	//implementation of abstract method "initialise" from interface
 	@Override
 	public void initialise() {
-		// TODO Auto-generated method stub
+		try {
+			//read username & password from textfields
+			user = usernameTf.getText().trim();
+			password = passwordTf.getText().trim();
+			String sql = "SELECT username, password FROM user_info where username = " +
+			"'" + user + "' and password = '"+password +"'";
+			ConnectionHelper.rs = ConnectionHelper.st.executeQuery(sql);
+			
+			int count = 0;
+			while (ConnectionHelper.rs.next()) {
+				count++;
+			}
+			
+			if (count==1) {
+				System.out.println("Username & Password Authenticated");
+				MainMenu myMenu = new MainMenu();	
+			} else {
+				System.out.println("Invalid Login Credentials ");
+			}
+		} catch (Exception ex) {
+			System.out.println("Exception: " +ex);
+		}
 
 	}
 
