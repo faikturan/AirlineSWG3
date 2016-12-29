76f38f32a0cb04eda56b3c5294f545f0939c65be
Faik TURAN <faikturan@users.noreply.github.com>
2016-12-29 20:08:48 +0300

    Add files via upload

---
+++ MainMenu.java
@@ -0,0 +1,70 @@
+package com.faikturan;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+public class MainMenu extends JFrame implements ActionListener {
+	private JLabel flightIdLbl;
+	private JLabel arrivalTimeLbl;
+	private JLabel deptTimeLbl;
+	private JLabel originLbl;
+	private JLabel destinationLbl;
+	private JLabel custIdLbl;
+	private JLabel fnameLbl;
+	private JLabel snameLbl;
+	private JLabel contactNoLbl;
+	private JLabel bookingType;
+	private JTextField flightIdTf;
+	private JTextField depTimeTf;
+	private JTextField arrTimeTf;
+	private JTextField originTf;
+	private JTextField destTf;
+	private JTextField custIdTf;
+	private JTextField fnameTf;
+	private JTextField surnameTf;
+	private JTextField contactNoTf;
+	private JTextField bookingTypeTf;
+	private JButton prevBtn;
+	private JButton nextBtn;
+	private JButton createBtn;
+	private JButton updateBtn;
+	private JButton deleteBtn;
+	private JButton closeBtn;
+	private JButton backupBtn;
+	private JButton reportBtn;
+	private static JLabel imageLabel;
+	private JLabel clockLbl;
+	private JLabel dateLbl;
+	private JLabel userLbl;
+	private JLabel loggedDescLbl;
+	private JLabel timeDescLbl;
+	private JLabel dateDescLbl;
+	private JLabel logoLbl;
+	
+	
+	
+	
+	
+
+	public static int count;
+
+	public static void initialise() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
