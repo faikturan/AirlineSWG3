249050f8f637d1410402da6b1467fb3e6a124a9b
Faik TURAN <faikturan@users.noreply.github.com>
2017-01-06 18:18:25 +0300

    Update MainMenu.java

--- MainMenu.java
+++ MainMenu.java
@@ -20,7 +20,7 @@ import javax.swing.JTextField;
 import javax.swing.Timer;
 import javax.swing.border.Border;
 
-import com.muratkorkmaz.Booking.Booking_Type;
+import com.faikturan.Booking.Booking_Type;
 
 import javax.swing.JFrame;
 
